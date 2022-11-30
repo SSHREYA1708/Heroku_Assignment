87acc0668edd9f7f0edefe36c102a70b90700532
SSHREYA1708 <119503647+SSHREYA1708@users.noreply.github.com>
2022-11-30 18:43:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+st.write("""
+# Whether the given number is even or odd
+""")
+
+#Get Input
+st.header('User Input Parameters')
+
+
+def user_input_features():
+    num = st.number_input("Number",step=1)
+    if(num%2==0):
+        return 'The number {num} is an even number.'
+    else:
+        return 'The number {num} is an odd number.'
+
+result=user_input_features()
+
+st.subheader('Result')
+st.write(result)
